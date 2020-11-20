3a37b2a137d6e3873f948200b3bf786603b2f6e9
Emanuele Ianni <emanuele.ianni@maersk.com>
2020-11-20 13:52:45 +0100

    duplicated code surrounded by if else

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -23,36 +23,8 @@ class GildedRose {
                         item.quality = item.quality + 1;
                     }
                 }
-            }else{
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.sellIn = item.sellIn - 1;
-                }
-
-                if (item.sellIn < 0) {
+            } else {
+                if (true) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.quality > 0) {
                             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -60,7 +32,77 @@ class GildedRose {
                             }
                         }
                     } else {
-                        item.quality = 0;
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+
+                            if (item.sellIn < 11) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+
+                            if (item.sellIn < 6) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+                        }
+                    }
+
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.sellIn = item.sellIn - 1;
+                    }
+
+                    if (item.sellIn < 0) {
+                        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (item.quality > 0) {
+                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                    item.quality = item.quality - 1;
+                                }
+                            }
+                        } else {
+                            item.quality = 0;
+                        }
+                    }
+                } else {
+                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                item.quality = item.quality - 1;
+                            }
+                        }
+                    } else {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+
+                            if (item.sellIn < 11) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+
+                            if (item.sellIn < 6) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+                        }
+                    }
+
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.sellIn = item.sellIn - 1;
+                    }
+
+                    if (item.sellIn < 0) {
+                        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (item.quality > 0) {
+                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                    item.quality = item.quality - 1;
+                                }
+                            }
+                        } else {
+                            item.quality = 0;
+                        }
                     }
                 }
 
