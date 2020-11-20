74a57407edf4bcb026db9bd578369b2ee9007e78
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2020-11-20 17:43:06 +0100

    r Optimize imports

--- src/test/java/com/gildedrose/GoldenMasterTest.java
+++ src/test/java/com/gildedrose/GoldenMasterTest.java
@@ -3,9 +3,7 @@ package com.gildedrose;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
 import java.io.PrintStream;
 
 public class GoldenMasterTest {
