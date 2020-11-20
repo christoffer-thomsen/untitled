d3e426d4ff818226c561f953694522c5a0869c2e
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2020-11-20 18:25:02 +0100

    Inline method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,23 +13,33 @@ class GildedRose {
                 continue;
             }
             if (item.name.equals("Aged Brie")) {
-                increaseQuality(item);
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
 
                 decreaseSellIn(item);
 
                 if (item.sellIn < 0) {
-                    increaseQuality(item);
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
                 }
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality < 50) {
-                    increaseQuality(item);
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
 
                     if (item.sellIn < 11) {
-                        increaseQuality(item);
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
 
                     if (item.sellIn < 6) {
-                        increaseQuality(item);
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
                 }
 
@@ -57,12 +67,6 @@ class GildedRose {
     private void decreaseQuality(Item item) {
         if (item.quality > 0) {
             item.quality = item.quality - 1;
-        }
-    }
-
-    private void increaseQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
         }
     }
 
