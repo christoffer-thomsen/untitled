f446aa7a9e285665c109fe9fe3a4a9f7db78422b
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2020-11-20 18:14:28 +0100

    Remove redundant condition

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -46,7 +46,6 @@ class GildedRose {
                 if (item.sellIn < 0) {
                     item.quality = 0;
                 }
-            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             } else {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
