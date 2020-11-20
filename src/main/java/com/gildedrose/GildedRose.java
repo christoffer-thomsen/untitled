ef05a801b4dd1e4bfdd8c470c9ae93d2dcf35205
Konstantinos Gkinis <konstantinos.gkinis@maersk.com>
2020-11-20 14:04:00 +0100

    Remove empty if statements

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -38,13 +38,7 @@ class GildedRose {
                 }
             } else {
                 if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    if (item.quality > 0) {
-                    }
 
-                    if (item.sellIn < 0) {
-                        if (item.quality > 0) {
-                        }
-                    }
                 } else {
                     if (item.quality > 0) {
                         item.quality = item.quality - 1;
