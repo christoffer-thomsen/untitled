97667a2bbed77113b4f6e6128611ff05294a717c
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2020-11-20 18:12:19 +0100

    Merge to else-if

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -43,19 +43,17 @@ class GildedRose {
                 if (item.sellIn < 0) {
                     item.quality = 0;
                 }
+            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             } else {
-                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                } else {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
                     if (item.quality > 0) {
                         item.quality = item.quality - 1;
-                    }
-
-                    item.sellIn = item.sellIn - 1;
-
-                    if (item.sellIn < 0) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
                     }
                 }
             }
