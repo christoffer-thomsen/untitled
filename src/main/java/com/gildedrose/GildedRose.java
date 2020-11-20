935807bde2097289cec642aeb7bf1ce70b6ba98d
Emanuele Ianni <emanuele.ianni@maersk.com>
2020-11-20 13:53:55 +0100

    split conditions

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -24,7 +24,7 @@ class GildedRose {
                     }
                 }
             } else {
-                if (true) {
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.quality > 0) {
                             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
