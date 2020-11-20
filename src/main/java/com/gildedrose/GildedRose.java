b791e660ce49711bd6d7a6edc760df5b3fc1cdab
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2020-11-20 18:21:43 +0100

    Extract method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -39,18 +39,20 @@ class GildedRose {
                     item.quality = 0;
                 }
             } else {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                decreaseQuality(item);
 
                 item.sellIn = item.sellIn - 1;
 
                 if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                    decreaseQuality(item);
                 }
             }
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 
